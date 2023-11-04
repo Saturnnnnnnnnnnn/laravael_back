1d515befe93dcbd02e7472a00b50a6e7ad06217b
Saturnnnnnnnnnnn <hamza.shermatov0504@gamil.com>
2023-11-04 11:18:24 +0300

    first commit

---
+++ database/migrations/2023_11_04_080850_create_person_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('person', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',100);
+            $table->string('address',200);
+            $table->bigInteger('phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('person');
+    }
+}
